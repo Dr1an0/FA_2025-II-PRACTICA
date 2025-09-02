07c546038deed6dd8181b099919f47065415c33f
James <adrianorruiz.25@gmail.com>
2025-09-02 15:31:33 -0500

    Subiendo ejercicio1_Semana1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera; //declarando variable
+
+            Console.Write("INGRESE SU NOMBRE: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("INGRESE SU CARRERA: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, BIENVENIDO AL CURSO DE FUDAMENTOS DE ALGORITMO DE {carrera}");
+
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ Semana_1_Python/Semana_1_Python.py
@@ -0,0 +1,8 @@
+def ejer1():
+    nombre = input("INGRESE SU NOMBREP: ")
+    carrera = input("INGRESE SU CARRERA: ")
+
+    print(f"\n{nombre}, BIENVENIDO A FUNDAMENTOS DE ALGORITMO DE {carrera}") #f = $ en C# (ES CONCATENAR)
+
+ejer1()
+
