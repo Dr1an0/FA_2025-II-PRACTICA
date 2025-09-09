62a34d33691092c3edee10d1776030092f629b99
Alexis <josephcuevasrafael@gmail.com>
2025-09-09 14:50:52 -0500

    ejer2 sem3

--- Semana_1_Python/Semana_1_Python.py
+++ Semana_1_Python/Semana_1_Python.py
@@ -4,5 +4,13 @@ def ejer1():
 
     print(f"\n{nombre}, BIENVENIDO A FUNDAMENTOS DE ALGORITMO DE {carrera}") #f = $ en C# (ES CONCATENAR)
 
-ejer1()
+def ejer2():
+    x = int(input("Ingresar el valor de x: "))
+    y = int(input("Ingresar el valor de y: "))
 
+    print("Suma: ", (x+y))
+    print("Resta: ", (x-y))
+    print("Multiplicación: ", (x*y))
+    print("División: ", (x/y))
+
+ejer2()
